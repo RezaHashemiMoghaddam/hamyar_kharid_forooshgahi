b81d9c5df98081b068824a30484831ea8b16647c
Reza Hashemi Moghaddam <rezahashemimoghaddam@gmail.com>
2026-08-20 16:33:17 +0330

    Delete hamyar_kharid_forooshgahi/backend/accounts directory

--- hamyar_kharid_forooshgahi/backend/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (('Project', {'fields': ('role', 'full_name', 'phone_number')}),)
-    add_fieldsets = UserAdmin.add_fieldsets + (('Project', {'fields': ('email', 'role', 'full_name', 'phone_number')}),)
--- hamyar_kharid_forooshgahi/backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import LoginView, LogoutView, MeView, CustomerRegisterView
-
-urlpatterns = [
-    path('register/', CustomerRegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('logout/', LogoutView.as_view()),
-    path('me/', MeView.as_view()),
-]
