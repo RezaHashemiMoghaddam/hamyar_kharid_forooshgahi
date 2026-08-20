007646883abb563ab3a2732b66829eeede7f3e86
Reza Hashemi Moghaddam <rezahashemimoghaddam@gmail.com>
2026-08-20 16:31:59 +0330

    Delete backend directory

--- backend/stores/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Inventory, InventoryImportLog, Product, Store, StoreAPIKey
-
-admin.site.register(Store)
-admin.site.register(Product)
-admin.site.register(Inventory)
-admin.site.register(InventoryImportLog)
-admin.site.register(StoreAPIKey)
